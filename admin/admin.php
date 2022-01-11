11d81c557dddf551905978d8aecb92fb5f01f03d
RejayMorada0 <rejaymorada0@gmail.com>
2022-01-11 21:15:41 +0800

    Add Files

--- admin/admin.php
+++ admin/admin.php
@@ -3,7 +3,6 @@
 
 session_start();
 
-	include("connection.php");
     include("admin_function.php");
 
 
@@ -134,6 +133,7 @@ session_start();
 
 
         <div class="main">
+            <!--HTML code to display data in tabular format-->
             <table>
                 <legend>ALL YEAR - ALL SEMESTER</legend>
                 <thead>
@@ -143,6 +143,23 @@ session_start();
                     <th>OFFERED STATUS</th>
                 </thead>
                 <tbody id="allsubtable">
+                    <!-- PHP CODE TO FETCH DATA FROM ROWS-->
+                    <?php   // LOOP TILL END OF DATA 
+                        while($rows=$result->fetch_assoc())
+                        {
+                    ?>
+                    <tr>
+                        <!--FETCHING DATA FROM EACH 
+                            ROW OF EVERY COLUMN-->
+                        <td><?php echo $rows['sub_code'];?></td>
+                        <td><?php echo $rows['sub_name'];?></td>
+                        <td><?php echo $rows['yr_and_sem'];?></td>
+                        <td><?php echo $rows['offer_stats'];?></td>
+                    </tr>
+                    <?php
+                        }
+                    ?>
+
 
                 </tbody>
 
@@ -181,7 +198,7 @@ session_start();
     <!--JAVASCRIPT-->
     <script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-fQybjgWLrvvRgtW6bFlB7jaZrFsaBXjsOMm/tB9LTS58ONXgqbR9W8oWht/amnpF" crossorigin="anonymous"></script>
-    <script src="admin.js"></script>
+   
 
 </body>
 
---
+++ admin/admin_function.php
@@ -0,0 +1,35 @@
+
+<?php
+
+//source: https://www.geeksforgeeks.org/how-to-fetch-data-from-localserver-database-and-display-on-html-table-using-php/
+// PHP code to establish connection
+// with the localserver
+
+// Server is localhost
+$dbhost = "localhost";
+
+// Username is root
+$dbuser = "root";
+$dbpass = "";
+
+// Database name
+$dbname = "adding_subjects_db";
+
+
+$mysqli = new mysqli($dbhost, $dbuser, $dbpass, $dbname);
+  
+
+// Checking for connections
+if ($mysqli->connect_error) {
+    die('Connect Error (' . 
+    $mysqli->connect_errno . ') '. 
+    $mysqli->connect_error);
+}
+  
+// SQL query to select data from database
+$sql = "SELECT * FROM all_subjects ORDER BY yr_and_sem ASC ";
+$result = $mysqli->query($sql);
+$mysqli->close(); 
+
+
+?>
