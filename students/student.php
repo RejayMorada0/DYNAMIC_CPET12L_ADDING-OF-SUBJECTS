fb384f3213dd62a7842c2da4a4334567aa7fbf48
RejayMorada0 <rejaymorada0@gmail.com>
2022-01-11 15:53:32 +0800

    Add Files

---
+++ students/student.php
@@ -0,0 +1,181 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="Mark Otto, Jacob Thornton, and Bootstrap contributors">
+    <meta name="generator" content="Hugo 0.88.1">
+    <title>Technological University of the Philippines</title>
+
+
+
+    <!--BOOTSTRAP-->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/css/bootstrap.min.css" integrity="sha384-zCbKRCUGaJDkqS1kPbPd7TveP5iyJE0EjAuZQTgFLD2ylzuqKfdKlfG/eSrtxUkn" crossorigin="anonymous">
+    <link rel="stylesheet" href="../signinlogin/css/bootstrap.min.css">
+
+    <!--CSS Custom -->
+    <link rel="stylesheet" href="css/student.css">
+
+    <!--FONTS-->
+    <link rel="stylesheet" href="../signinlogin/css/fonts.css">
+
+    <!--ICONS-->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.7.1/font/bootstrap-icons.css">
+    <link rel="icon" href="../signinlogin/images/tuplogo.png">
+
+</head>
+
+<body onresize="openpage()">
+    <nav class="navbar navbar-expand-lg" id="tupcnav">
+        <img src="../signinlogin/images/tuplogo.png" width="50" height="50" alt="tuplogo">
+        <a class="navbar-brand" href="index.html" id="atech">TECHNOLOGICAL UNIVERSITY <br> OF THE PHILIPPINES</a>
+
+        <div class="collapse navbar-collapse" id="navbarToggleExternalContent">
+
+            <form class="d-flex" id="logoutflex">
+                <ul class="navbar-nav mr-auto">
+                    <li class="nav-item">
+                        <a class="nav-link" href="../signinlogin/index.php" id="logoutbutton">Logout</a>
+                    </li>
+                </ul>
+
+
+            </form>
+        </div>
+
+        <i class="bi bi-person-circle" onclick="details_hide_show()"></i>
+
+    </nav>
+
+
+    <section id="section">
+        <div class="studentinfostatus" id="studentinfostatus">
+            <div class="studentinfo">
+                <div class="info">
+                    <label>Student Number:</label>
+                    <label>Name:</label>
+                    <label>Year Course and Section:</label>
+                </div>
+                <div class="data">
+                    <label id='studentnumber1'>TUPC-19-0001</label>
+                    <label id='fullname1'>JUAN DELA CRUZ</label>
+                    <label id='section1'>BET-COET-4B</label>
+                </div>
+
+            </div>
+
+            <div class="studentstatus">
+                <div class="info">
+                    <label>Status:</label>
+                </div>
+                <div class="statusCenter">
+                    <label id='status1'>PROCESSED</label>
+                </div>
+                <div class="statusCenter">
+                    <a href="" target="_blank"> <i class="bi bi-download"></i> Download PDF</a>
+                </div>
+            </div>
+            <ul class="navbar-nav mr-auto">
+                <li class="nav-item">
+                    <a class="nav-link" href="../signinlogin/index.php" id="logoutbutton1">Logout</a>
+                </li>
+            </ul>
+
+
+        </div>
+    </section>
+
+    <section>
+        <div class="subject">
+            <p>REQUEST OF SUBJECT</p>
+        </div>
+
+        <div>
+            <form class="needs-validation" name="add_grades" action="javascript:addGrades();">
+                <div class="form-row">
+                    <div class="col-md-4 mb-3">
+                        <label for="validationTooltip01">Subject Code</label>
+                        <input type="text" class="form-control" id="validationTooltip01" required>
+
+                    </div>
+                    <div class="col-md-4 mb-3">
+                        <label for="validationTooltip02">Select Grade Here</label>
+                        <input type="number" class="form-control" id="validationTooltip02" step=0.01 required>
+                    </div>
+
+                    <div class="gradebutton">
+                        <input class="btn btn-primary" type="submit" value="Update Grade" id="updategrade">
+                        <!--<button class="btn btn-primary" type="submit">Submit</button>-->
+                    </div>
+            </form>
+            </div>
+
+
+
+
+            <form>
+                <div class="tab-content" id="myTabContent">
+
+                    <div class="tableborder">
+                        <table>
+                            <legend>ALL YEAR - ALL SEMESTER</legend>
+                            <thead>
+                                <th>SUBJECT CODE</th>
+                                <th>SUBJECT NAME</th>
+                                <th>YEAR AND SEMESTER</th>
+                                <th>GRADES</th>
+                            </thead>
+                            <tbody id="fyfs">
+
+                            </tbody>
+
+                        </table>
+                    </div>
+                    <div class="imgforgrades">
+                        <p>*Input your previous grades on the following academic year and upload a photo from ers as a proof.</p>
+                        <input type="file" id="gradesfile" accept=".jpg, .png, .jpeg">
+                    </div>
+
+
+                    <div class="submitbutton">
+                        <form name="student_grade" action="javascript:studentGrade();">
+                            <!--<input type="submit" value="SUBMIT REQUEST">-->
+                        </form>
+                    </div>
+
+
+    </section>
+
+    <div class="submitbutton">
+        <form name="student_grade" action="javascript:studentGrade();">
+            <input type="submit" value="SUBMIT REQUEST" id="submitbtn">
+        </form>
+    </div>
+
+
+    <script src="../signinlogin/js/bootstrap.bundle.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-fQybjgWLrvvRgtW6bFlB7jaZrFsaBXjsOMm/tB9LTS58ONXgqbR9W8oWht/amnpF" crossorigin="anonymous"></script>
+    <script>
+        function openpage() {
+            var x = document.getElementById("section");
+            if (window.innerWidth > 1000) {
+                x.style.display = "grid";
+            }
+        }
+
+        function details_hide_show() {
+            var x = document.getElementById("section");
+            if (x.style.display === "none") {
+                x.style.display = "grid";
+            } else {
+                x.style.display = "none";
+            }
+        }
+    </script>
+
+</body>
+
+</html>
