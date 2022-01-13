05c9c1322b5e8352c02073dc1ba949cc4b6da639
RejayMorada0 <rejaymorada0@gmail.com>
2022-01-13 15:41:00 +0800

    Add Files

--- pic/pic.php
+++ pic/pic.php
@@ -122,11 +122,14 @@ session_start();
 
         <script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-fQybjgWLrvvRgtW6bFlB7jaZrFsaBXjsOMm/tB9LTS58ONXgqbR9W8oWht/amnpF" crossorigin="anonymous"></script>
+        
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.7.0/jquery.min.js" type="text/javascript"></script>
+        
         <script>
             
             $(document).ready (function () {
                     var updater = setTimeout (function () {
-                        $('body#ajax_func').load ('admin.php', 'update=true');
+                        $('body#ajax_func').load ('pic.php', 'update=true');
                     }, 100);
                 });
 
