39fd954ae6109c52a917bd1426921861d9d25e17
RejayMorada0 <rejaymorada0@gmail.com>
2022-01-13 16:05:43 +0800

    Add Files

--- admin/admin_function.php
+++ admin/admin_function.php
@@ -42,7 +42,7 @@ $mysqli->close();
 
 
 // source: https://stackoverflow.com/questions/20738329/how-to-call-a-php-function-on-the-click-of-a-button
-// ADD and REMOVE FUNCTION
+// ADD and REMOVE and CHANGE FUNCTION
 
 if(array_key_exists('removeAction', $_POST)) {
     removeAction();
